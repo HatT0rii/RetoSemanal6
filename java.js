fb8fdb0570bcbbc7cbad9b525337b76f8a3271fd
Bryan <alex.ari190@gmail.com>
2021-05-02 19:09:48 -0500

    fisrt

---
+++ java.js
@@ -0,0 +1,28 @@
+let arreglo = [];
+
+let ingreso= document.querySelector("#add");
+
+let nuevo = document.querySelector("#New");
+
+let divUl = document.querySelector("#ul");
+
+let divLi = document.getElementsByTagName("li");
+
+nuevo.addEventListener('click',function(){
+    let tarea = ingreso.value;
+    if(tarea === ""){
+        alert("No ha ingresado Tarea")
+    }else{
+    let li= document.createElement('li');
+        li.innerHTML= tarea;
+        divUl.append(li);
+        arreglo.push(tarea);
+        console.log(arreglo);
+    };
+});
+
+divUl.addEventListener('click',function(evento){
+    if(evento.target.tagName == 'LI'){
+        evento.target.classList.toggle('checked');
+    };
+});
